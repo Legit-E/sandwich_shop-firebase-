15806e1a19ffbebdbf93d469ffea2ce4da0eade4
ehan1736 <ehan1736@gmail.com>
2025-10-17 02:01:56 +0000

    Create a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 // runApp(const MyApp());
 
 // class MyApp extends StatelessWidget {
